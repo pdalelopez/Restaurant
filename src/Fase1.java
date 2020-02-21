b71b5e7e00376955aaf59984290c53a008f26bbd
formacio <formacio@BARCELO-EHVPRGP>
2020-02-21 11:26:39 +0100

    Creating project and Fase1 finish

---
+++ src/Fase1.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Fase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int five, ten, twenty, fifty, hundred, twoHundred, fiveHundred;
+		
+		five=5;
+		ten=10;
+		twenty=20;
+		fifty=50;
+		hundred=100;
+		twoHundred=200;
+		fiveHundred=500;
+		
+		double totalPrice;
+		
+		String menu[] = {"Pizza","Burguer","Nachos","Grilled Chicken","Soup"};
+		double prices[] = {10.50,12.00,8.5,14.20,6.50};
+		
+		System.out.println(Arrays.toString(menu));
+	}
+
+}
